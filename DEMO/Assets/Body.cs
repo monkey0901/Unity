030ec4b15a9a55d9f85cf37c3479256cdcab7ab0
monkey0901 <1284757581@qq.com>
2019-06-27 23:22:06 +0800

    设置身体，以及吃掉食物后身体的增长

---
+++ DEMO/Assets/Body.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Body : MonoBehaviour
+{
+    //下一个身子的引用
+    public Body next;
+
+    //使当前身子移动
+    public void MOve(Vector3 pos)
+    {
+        //来记录移动前的位置
+        Vector3  nextPos = transform.position;
+        //移动当前身子
+        transform.position = pos;
+        //如果后面还有身子
+        if (next != null)
+        {
+            //移动后面的身子
+            next.MOve(nextPos);
+        }
+    }
+}
--- DEMO/Assets/HeadCtrl.cs
+++ DEMO/Assets/HeadCtrl.cs
@@ -12,9 +12,19 @@ public enum HeadDir
 
 public class HeadCtrl : MonoBehaviour
 {
-    //移动速度，米/秒
-    public float speed;
+    //食物预设体
+    public GameObject FoodPrefab;
+    
+    //身子预设体
+    public GameObject BodyPrefab;
+    
+    //第一节身子的引用
+    private Body _FirstBody;
+    private Body _LastBody;
 
+    //移动速度，米/秒 
+    public float speed;
+    
     //计时器用来记录移动时间
     private float _Timer = 0f;
 
@@ -24,12 +34,65 @@ public class HeadCtrl : MonoBehaviour
     //蛇头改变的方向
     private HeadDir _NextDir;
 
+    private bool _IsOver = false;
+
+    public void CreateFood()
+    {
+        float x = Random.Range(-9.5f, 9.5f);
+        float z = Random.Range(-9.5f, 9.5f);
+        GameObject obj = Instantiate(FoodPrefab,new Vector3 (x, 0f, z), Quaternion.identity) as GameObject;
+    }
+
+
+    //触发事件，当触发开始时
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("Bound"))
+        {
+            _IsOver = true;
+
+        }
+        if(other.tag.Equals("Food"))
+        {
+            Destroy(other.gameObject);
+            Grow();
+            CreateFood();
+        }
+    }
+    private void Grow()
+    {
+        //动态创建身子
+        GameObject obj = Instantiate(BodyPrefab, new Vector3(1000f, 1000f, 1000f), Quaternion.identity);
+        //获取身子上的body脚本
+        Body b = obj.GetComponent<Body>();
+        if(_FirstBody == null)
+        {
+            //当前创建的身子就是第一节身子
+            _FirstBody = b;
+        }
+        if(_LastBody != null)
+        {
+            //新创建的身子设置在最后一节身子后面
+            _LastBody.next = b;
+        }
+        //更新最后一节身子
+        _LastBody = b;
+    }
+    private void Start ()
+    {
+        //游戏开始时需要有一个食物
+        CreateFood();
+    }
     // Update is called once per frame
     private void Update()
     {
-        Turn();
-        Move();
+        if (!_IsOver)
+        {
+            Turn();
+            Move();
+        }
     }
+
     //控制蛇头转向
     private void Turn()
     {
@@ -99,11 +162,18 @@ public class HeadCtrl : MonoBehaviour
                     break;
 
             }
-
+            //记录头部移动之前的位置
+            Vector3 nextPos = transform.position;
             //向前移动一个单位
             transform.Translate(Vector3.forward);
             //重置计时器
             _Timer = 0f;
+            //如果有身子
+            if(_FirstBody != null)
+            {
+                //就让第一节身子移动
+                _FirstBody.MOve(nextPos);
+            }
         }
     }
 }
